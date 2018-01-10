0879ed1c1b71b9ad5ba399ba515e14cf0b7f76e7
FlyingHail <flyinghail@msn.com>
2018-01-10 16:58:26 +0800

    fix config

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
-require __DIR__ . '/../autoload.php';
+// Absolute path to the application base folder
+defined('BASE_PATH') || define('BASE_PATH', dirname(__DIR__));
+
+require BASE_PATH . '/vendor/autoload.php';
 
 Hail\Framework::listen();
